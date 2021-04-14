bd6e3876d24106e224fbda1229023ff24d40e60d
summer <ruanjiayu@fenxianglife.com>
2021-04-14 15:05:00 +0800

    完成两数之和

--- src/main/java/leetcode/cn/No_1_twoSum.java
+++ src/main/java/leetcode/cn/No_1_twoSum.java
@@ -1,5 +1,11 @@
 package leetcode.cn;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Description: 两数之和
  * @Author: Summer
@@ -8,5 +14,24 @@ package leetcode.cn;
  */
 public class No_1_twoSum {
 
+    @Test
+    public void test() {
+        int[] nums = new int[]{3, 2, 4};
+        int target = 6;
+        int[] ints = twoSum(nums, target);
+        System.out.println(Arrays.toString(ints));
+    }
 
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> maps = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (maps.containsKey(target - nums[i])) {
+                return new int[]{maps.get(target - nums[i]), i};
+            }
+
+            maps.put(nums[i], i);
+        }
+
+        return new int[2];
+    }
 }
